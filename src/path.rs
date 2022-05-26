700a24e7ee57546143a1a1f36adee4ed302cf40f
Jeong YunWon <jeong@youknowone.org>
2022-05-26 09:07:19 +0900

    Fix clippy

--- src/path.rs
+++ src/path.rs
@@ -14,7 +14,7 @@ where
 {
     /// Any [crate::ext::GetPath] automatically implements [crate::ext::GetIdent]
     fn get_ident(&self) -> Option<&Ident> {
-        self.get_path().map(|p| p.get_ident()).flatten()
+        self.get_path().and_then(|p| p.get_ident())
     }
 }
 
